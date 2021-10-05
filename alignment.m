6ed3c2356575e7a554583d167a2dcf2f638abf3b
Lisa <vdyklisa@gmail.com>
2021-10-05 22:16:01 +0200

    Pre-processing steps for alignment algorithm been updated

--- alignment.m
+++ alignment.m
@@ -1,4 +1,6 @@
 %% This script calculates the correlation of pixels across the time-lapse microscopy and outputs the time series aligned with respect to one another
+%   
+%   Based of the ViCAR, an automatic registration algorithm 
 %
 %   Lisa van Dyk
 %   University of Cape Town
@@ -6,73 +8,44 @@
 %   02 October 2021
 
 %%
+close all;
 
 I = imread('2021-05-22_Experiment-01_NRG_Msm-GFP_Exp1.czi - 2021-05-22_Experiment-01_NRG_Msm-GFP_Exp1_t001_c001.jpg');
+%Convert image into grey-scale image
+I_gray = rgb2gray(I);
+%Increase contrast of image for a clearer distinction of edges
+I_contrast = imadjust(I_gray,stretchlim(I_gray),[]);
 
-%I_gray = rgb2gray(I);
-%imshow(I_gray);
-%title('Original phase image');
-
-%Set up variables
-
-tol = 1;
-i = 1;
-j = 1;
-
-
-figure;
-J5 = imcrop(I, [80 200 1456 1304]);
-imshow(J5);
-title('Ideal cropped image');
-
-[numRows, numCols] = size(J5);
-
-corr_matrix = zeros(numRows-1, numCols-1);
-
-for i = 1:(numRows-1)  
-   for j = 1:(numCols-1)
-      deltaAB = J5(i,j) - J5(i+1,j);
-      deltaAC = J5(i,j) - J5(i,j+1);
-      corr_matrix(i,j) = fmap(deltaAB,deltaAC, tol);
-   end
-end
-
-%{
-subplot(2,2,1);
-J1 = imcrop(I,[1 1 1376 1104]);
-imshow(J1);
-title('Cropped image of first quarter');
-
-subplot(2,2,2);
-J2 = imcrop(I, [1377 1 2752 1104]);
-imshow(J2);
-title('Cropped image of section II');
-
-subplot(2,2,3);
-J3 = imcrop(I, [1 1105 1376 2208]);
-imshow(J3);
-title('Cropped image of section III');
-
-subplot(2,2,4);
-J4 = imcrop(I, [1377 1105 2752 2208]);
-imshow(J4);
-title('Cropped image of section IV');
-
-%}
-
-%{
+%calculate threshold image of edges only
 [numRows, numCols] = size(I_gray);
 
-temp_image = false([numRows, numCols]);
+mask = false(numRows, numCols);
+%set threshold value before histogram exponential curve in pixel intenisty
+T = 60;
 
 for i = 1:numRows
    for j = 1:numCols
-      if I(i,j) < 70
-          temp_image(i,j) = true;
+      if I(i,j) > T
+          mask(i,j) = true;
       end
    end
 end
 
+
 figure;
-imshow(temp_image);
+subplot(1,2,1);
+imhist(I_contrast);
+
+subplot(1,2,2);
+imshow(mask);
+title(['Resulting binary mask for T = ', num2str(T)]);
+
+figure;
+subplot(1,2,1);
+imshow(I_gray);
+title("Phase contrast image in Grey");
+
+subplot(1,2,2);
+imshow(I_contrast);
+title("Increased contrast of image");
 %}
